5589c1646483f988e4d4b445d8f730f7489ad97c
lisa710junyi <83738368+lisa710junyi@users.noreply.github.com>
2021-12-12 15:58:33 -0500

    push

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 import express from "express"
 import cors from "cors"
 import foods from './api/foods.route.js'
+import path from "path"
+
+
 
 const app = express()
 
@@ -9,5 +12,24 @@ app.use(express.json())
 
 app.use("/api/v1/foods",foods)
 app.use("*",(req,res)=>res.status(404).json({error:"not found"}))
+
+/*-- DEPLOYMENT -*/
+
+const __dirname=path.resolve()
+if(process.env.NODE_ENV==='production'){
+    app.use(express.static(path.join(__dirname, "/frontend/build")));
+    app.get('*',(req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html")))
+}else{
+    console.log(process.env.NODE_ENV)
+    app.get("/", (req, res) => {
+        res.send("API is running..");
+      }); 
+}
+
+
+//-------------------
+
+
 
 export default app // import to the file that get access to database
