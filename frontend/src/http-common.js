10b4b2d297be5b1a3f5f1767fa7992a464cd6c18
lisa710junyi <83738368+lisa710junyi@users.noreply.github.com>
2023-01-27 15:26:30 -0500

    deploy fix

--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://mern-begin.herokuapp.com/api/v1/foods/",
+  baseURL: "https://restaurantrev.herokuapp.com/api/v1/foods/",
   headers: {
     "Content-type": "application/json"
   }
